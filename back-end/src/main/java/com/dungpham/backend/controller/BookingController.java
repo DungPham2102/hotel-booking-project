2428041c0879172d0775be154abb1005215174f6
DungPham2102 <dungpham21022003@gmail.com>
2024-04-14 20:20:59 +0700

    uncomment function

--- back-end/src/main/java/com/dungpham/backend/controller/BookingController.java
+++ back-end/src/main/java/com/dungpham/backend/controller/BookingController.java
@@ -19,41 +19,41 @@ import java.util.List;
 @RequestMapping("/bookings")
 public class BookingController {
 
-//    private final IBookingService bookingService;
-//
-//    @GetMapping("/all-bookings")
-//    public ResponseEntity<List<BookingResponse>> getAllBookings() {
-//        List<BookedRoom> bookings = bookingService.getAllBookings();
-//        List<BookingResponse> bookingResponses = new ArrayList<>();
-//        for(BookedRoom booking : bookings){
-//            BookingResponse bookingResponse = getBookingResponse(booking);
-//            bookingResponses.add(bookingResponse);
-//        }
-//        return ResponseEntity.ok(bookingResponses);
-//    }
-//
-//    @GetMapping("/confirmation/{confirmationCode}")
-//    public ResponseEntity<?> getBookingByConfirmationCode( @PathVariable String confirmationCode) {
-//        try{
-//            BookedRoom booking = bookingService.findByBookingConfirmationCode(confirmationCode);
-//            BookingResponse bookingResponse = getBookingResponse(booking);
-//            return ResponseEntity.ok(bookingResponse);
-//        }catch(ResourceNotFoundException ex){
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-//
-//        }
-//    }
-//
-//    public ResponseEntity<?> saveBooking(@PathVariable Long roomId,
-//                                         @RequestBody BookedRoom bookingRequest){
-//        try{
-//            String confirmationCode = bookingService.saveBooking(roomId, bookingRequest);
-//            return ResponseEntity.ok("Room booked successfully. Confirmation code: " + confirmationCode);
-//        }catch(InvalidBookingRequestException ex){
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
-//
-//        }
-//    }
+    private final IBookingService bookingService;
+
+    @GetMapping("/all-bookings")
+    public ResponseEntity<List<BookingResponse>> getAllBookings() {
+        List<BookedRoom> bookings = bookingService.getAllBookings();
+        List<BookingResponse> bookingResponses = new ArrayList<>();
+        for(BookedRoom booking : bookings){
+            BookingResponse bookingResponse = getBookingResponse(booking);
+            bookingResponses.add(bookingResponse);
+        }
+        return ResponseEntity.ok(bookingResponses);
+    }
+
+    @GetMapping("/confirmation/{confirmationCode}")
+    public ResponseEntity<?> getBookingByConfirmationCode( @PathVariable String confirmationCode) {
+        try{
+            BookedRoom booking = bookingService.findByBookingConfirmationCode(confirmationCode);
+            BookingResponse bookingResponse = getBookingResponse(booking);
+            return ResponseEntity.ok(bookingResponse);
+        }catch(ResourceNotFoundException ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+
+        }
+    }
+
+    public ResponseEntity<?> saveBooking(@PathVariable Long roomId,
+                                         @RequestBody BookedRoom bookingRequest){
+        try{
+            String confirmationCode = bookingService.saveBooking(roomId, bookingRequest);
+            return ResponseEntity.ok("Room booked successfully. Confirmation code: " + confirmationCode);
+        }catch(InvalidBookingRequestException ex){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+
+        }
+    }
 
 
 }
